24ab4ad3f7f727b225fcc173f8055eaeb4dfd845
Nick Kallen <nickkallen@me.com>
2017-09-13 15:54:08 -0400

    More logic in SceneView subclass

--- Multiplayer/GameViewController.swift
+++ Multiplayer/GameViewController.swift
@@ -10,45 +10,19 @@ class GameViewController: UIViewController {
     @IBOutlet weak var sequenceLabel: UILabel!
     @IBOutlet var nonRecordingView: UIView!
 
-    var multiplayer: GameKitMultiplayer<MyInputInterpreter>!
-    var inputInterpreter: MyInputInterpreter!
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        inputInterpreter = MyInputInterpreter(scene: sceneView.scene!)
-        multiplayer = GameKitMultiplayer(inputInterpreter: inputInterpreter)
-        sceneView.multiplayer = multiplayer
 //        _ = multiplayer.localState.register(cameraNode, priority: 1) { registered in
 //            self.multiplayer.localState.input(.create(.pov, id: registered.id))
 //        }
 
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        sceneView.addGestureRecognizer(tapGesture)
+
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         screenRecordingManager = ScreenRecordingManager(frame: view.frame, view: nonRecordingView)
-    }
-
-    // Mark: Interaction
-
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        createBox()
-    }
-
-    func createBox() {
-        let box = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
-        box.firstMaterial = SCNMaterial.material(withDiffuse: UIColor.blue.withAlphaComponent(0.5))
-        let node = SCNNode(geometry: box)
-        node.simdPosition = float3(0, 10, 0)
-        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
-        _ = multiplayer.localState.register(node, priority: 1) { registered in
-            self.multiplayer.localState.input(.create(.pov, id: registered.id))
-        }
-        sceneView.scene?.rootNode.addChildNode(node)
     }
 
     // MARK: - Recording
--- Multiplayer/Scene.swift
+++ Multiplayer/Scene.swift
@@ -2,7 +2,8 @@ import Foundation
 import SceneKit
 
 class SceneView: SCNView, SCNSceneRendererDelegate {
-    weak var multiplayer: GameKitMultiplayer<MyInputInterpreter>!
+    var multiplayer: GameKitMultiplayer<MyInputInterpreter>!
+    var inputInterpreter: MyInputInterpreter!
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -12,11 +13,36 @@ class SceneView: SCNView, SCNSceneRendererDelegate {
         showsStatistics = true
         backgroundColor = UIColor.black
 
+        inputInterpreter = MyInputInterpreter(scene: scene!)
+        multiplayer = GameKitMultiplayer(inputInterpreter: inputInterpreter)
+
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        addGestureRecognizer(tapGesture)
+
         delegate = self
     }
 
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         renderer.isPlaying = true
         multiplayer.renderedFrame(updateAtTime: time, of: scene!)
+    }
+
+    // Mark: Interaction
+
+    @objc
+    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+        createBox()
+    }
+
+    func createBox() {
+        let box = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
+        box.firstMaterial = SCNMaterial.material(withDiffuse: UIColor.blue.withAlphaComponent(0.5))
+        let node = SCNNode(geometry: box)
+        node.simdPosition = float3(0, 10, 0)
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        _ = multiplayer.localState.register(node, priority: 1) { registered in
+            self.multiplayer.localState.input(.create(.pov, id: registered.id))
+        }
+        scene!.rootNode.addChildNode(node)
     }
 }
